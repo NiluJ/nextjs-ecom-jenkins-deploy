b43d83c424eddbda8d7ed4c808679f6d83194771
Nilesh Jawle <jawleniluwork@gmail.com>
2026-03-27 15:30:46 +0530

    Added debug logging for Jenkins CI deployment validation

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -111,7 +111,7 @@ export const getStaticProps = async ({ params: { slug }}) => {
   const products = await client.fetch(productsQuery);
 
   console.log(product);
-
+  console.log("Backend fetched product successfully");
   return {
     props: { products, product }
   }
